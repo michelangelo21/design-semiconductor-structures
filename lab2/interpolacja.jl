a8f63fffc7e236971ca7228e2a8fd1e875b15af3
Michał Łukomski <michal.lukomski21@gmail.com>
2022-03-29 16:17:38 +0200

    interpolation

--- lab2/interpolacja.jl
+++ lab2/interpolacja.jl
@@ -1,13 +1,13 @@
 using Plots
 plotlyjs()
 
-q_AB(x, q_A, q_B, C=0) = x * q_A + (1 - x) * q_B + x * (1 - x) * C
+interpolation(x, q_A, q_B, C=0) = x * q_A + (1 - x) * q_B + x * (1 - x) * C
 
 # Ga_x In_(1-x) | P
 Eg_GaP = 2.886 # eV
 Eg_InP = 1.4236 # eV
-C = 0.65
+C = 0.65 # bowing
 
-Eg_GaInP(x) = q_AB(x, Eg_GaP, Eg_InP, C)
+Eg_GaInP(x) = interpolation(x, Eg_GaP, Eg_InP, C)
 
-plot(0:0.01:1, Eg_GaInP)
+plot(0:0.01:1, Eg_GaInP, xlabel="x", ylabel="E_g [eV]", title="Gaₓ In₍₁₋ₓ₎ P")
